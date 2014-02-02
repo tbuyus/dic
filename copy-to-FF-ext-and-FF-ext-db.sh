92c195ee499c5d6d6b8f9165174a1d4750e8a827
tbuyus <tbuyus@tbuyus-ME05.(none)>
2014-02-02 15:02:35 +0000

    changed directory refs in copy-to-... shell script after adding dic project to git hub and having pulled it into this new dic/ subdirectory within the original directory
    have decided to treat of as a sight word and remove
    the voiced f from the augmentation scheme it only
    occurs twice 'of' and 'thereof' - I will need to
    update the ffextension to remove this
    [It was just too painfull getting the alternate f to
    capitalise properly and when 'of' is so frequently
    capitalised in titles and headings this was looking
    yuk

--- copy-to-FF-ext-and-FF-ext-db.sh
+++ copy-to-FF-ext-and-FF-ext-db.sh
@@ -1,5 +1,5 @@
-cp spell.sqlite ../ext
-cp spell.sqlite ../ext/db
+cp spell.sqlite ../../ext
+cp spell.sqlite ../../ext/db
 
-cd ../ext/db/
+cd ../../ext/db/
 . lmake-db-xpi.sh 
--- createTableDictionary.sql
+++ createTableDictionary.sql
@@ -1,4 +1,4 @@
 CREATE TABLE dictionary(tradspell VARCHAR PRIMARY KEY, augmented VARCHAR ,  freq integer, rank real);
 select * from dictionary
 where 
-tradspell like '%ssess%';
+tradspell like 'of';
